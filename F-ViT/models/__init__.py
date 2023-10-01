64e02abc612005849fca35054e141d287d701fd3
wusize <wusize@sensetime.com>
2023-10-01 18:03:18 +0800

    'debug'

--- F-ViT/models/__init__.py
+++ F-ViT/models/__init__.py
@@ -2,3 +2,4 @@ from .custom_losses import CustomCrossEntropyLoss  # noqa
 from .evaclip_vit import EvaCLIPViT   # noqa
 from .fvit_head import FViTRoIHead, FViTBBoxHead, FViTTransferBBoxHead   # noqa
 from .fvit import FViT # noqa
+from .custom_rpn_head import CustomRPNHead  # noqa
---
+++ F-ViT/models/custom_rpn_head.py
@@ -0,0 +1,45 @@
+import torch.nn as nn
+from mmcv.cnn import ConvModule
+from mmdet.models.builder import HEADS
+from mmdet.models.dense_heads import RPNHead
+
+
+@HEADS.register_module()
+class CustomRPNHead(RPNHead):
+    def __init__(self,
+                 norm_cfg=None,
+                 *args,
+                 **kwargs):
+        super().__init__(*args, **kwargs)
+        self.norm_cfg = norm_cfg
+
+    def _init_layers(self):
+        """Initialize layers of the head."""
+        if self.num_convs > 1:
+            rpn_convs = []
+            for i in range(self.num_convs):
+                if i == 0:
+                    in_channels = self.in_channels
+                else:
+                    in_channels = self.feat_channels
+                # use ``inplace=False`` to avoid error: one of the variables
+                # needed for gradient computation has been modified by an
+                # inplace operation.
+                rpn_convs.append(
+                    ConvModule(
+                        in_channels,
+                        self.feat_channels,
+                        3,
+                        padding=1,
+                        inplace=False,
+                        norm_cfg=self.norm_cfg
+                    ))
+            self.rpn_conv = nn.Sequential(*rpn_convs)
+        else:
+            self.rpn_conv = nn.Conv2d(
+                self.in_channels, self.feat_channels, 3, padding=1)
+        self.rpn_cls = nn.Conv2d(self.feat_channels,
+                                 self.num_base_priors * self.cls_out_channels,
+                                 1)
+        self.rpn_reg = nn.Conv2d(self.feat_channels, self.num_base_priors * 4,
+                                 1)
