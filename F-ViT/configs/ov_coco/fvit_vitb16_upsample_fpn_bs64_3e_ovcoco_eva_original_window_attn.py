42dacbb673ebc525676f99969fca9df0189b0a29
wusize <wusize@sensetime.com>
2023-11-12 17:02:23 +0800

    window_attn_det

---
+++ F-ViT/configs/ov_coco/fvit_vitb16_upsample_fpn_bs64_3e_ovcoco_eva_original_window_attn.py
@@ -0,0 +1,228 @@
+find_unused_parameters = True
+num_classes = 65
+class_weight = [
+    1.0, 1.0, 1.0, 1.0, 0, 0, 1.0, 1.0, 1.0, 1.0, 1.0, 0, 0, 1.0, 1.0, 0, 0,
+    1.0, 1.0, 1.0, 1.0, 0, 1.0, 0, 1.0, 1.0, 1.0, 0, 1.0, 0, 1.0, 1.0, 0, 1.0,
+    0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0, 1.0, 0, 1.0, 1.0,
+    1.0, 1.0, 1.0, 1.0, 0, 1.0, 1.0, 1.0, 0, 1.0, 1.0, 1.0, 1.0, 0, 1.0, 0.6
+]
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+model = dict(
+    type='FViT',
+    backbone=dict(
+        type='EvaCLIPViT',
+        model_name='EVA02-CLIP-B-16',
+        pretrained='checkpoints/EVA02_CLIP_B_psz16_s8B.pt',
+        norm_cfg=norm_cfg,
+        out_indices=[3, 5, 7, 11],
+        window_block_indexes=[0, 1, 3, 4, 6, 7, 9, 10],
+    ),
+    neck=dict(
+        type='FPN',
+        in_channels=[768, 768, 768, 768],
+        out_channels=256,
+        num_outs=5,
+        norm_cfg=norm_cfg),
+    rpn_head=dict(
+        type='RPNHead',
+        in_channels=256,
+        feat_channels=256,
+        anchor_generator=dict(
+            type='AnchorGenerator',
+            scales=[8],
+            ratios=[0.5, 1.0, 2.0],
+            strides=[4, 8, 16, 32, 64]),
+        bbox_coder=dict(
+            type='DeltaXYWHBBoxCoder',
+            target_means=[0.0, 0.0, 0.0, 0.0],
+            target_stds=[1.0, 1.0, 1.0, 1.0]),
+        loss_cls=dict(
+            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
+        loss_bbox=dict(type='L1Loss', loss_weight=1.0),
+        num_convs=2),
+    roi_head=dict(
+        type='FViTRoIHead',
+        bbox_roi_extractor=dict(
+            type='SingleRoIExtractor',
+            roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=0),
+            out_channels=256,
+            featmap_strides=[4, 8, 16, 32]),
+        bbox_head=dict(
+            type='FViTBBoxHead',
+            in_channels=256,
+            fc_out_channels=512,
+            roi_feat_size=7,
+            num_classes=num_classes,
+            bbox_coder=dict(
+                type='DeltaXYWHBBoxCoder',
+                target_means=[0.0, 0.0, 0.0, 0.0],
+                target_stds=[0.1, 0.1, 0.2, 0.2]),
+            reg_class_agnostic=True,
+            loss_cls=dict(
+                type='CustomCrossEntropyLoss',
+                use_sigmoid=False,
+                loss_weight=1.0,
+                class_weight=class_weight),
+            loss_bbox=dict(type='L1Loss', loss_weight=1.0),
+            norm_cfg=norm_cfg,
+            fixed_temperature=0,
+            learned_temperature=50.0,
+            vlm_temperature=75.0,
+            alpha=0.1,
+            beta=0.8,
+            class_embed=
+            'datasets/embeddings/coco_with_background_evaclip_vitb_16.pt',
+            seen_classes='datasets/mscoco_seen_classes.json',
+            all_classes='datasets/mscoco_65_classes.json',
+            num_shared_convs=4,
+            num_shared_fcs=2,
+            num_cls_fcs=1,
+            num_reg_fcs=1),
+        vlm_roi_extractor=dict(
+            type='SingleRoIExtractor',
+            roi_layer=dict(
+                type='RoIAlign',
+                output_size=1,
+                sampling_ratio=0,
+                use_torchvision=True),
+            out_channels=512,
+            featmap_strides=[16])),
+    train_cfg=dict(
+        rpn=dict(
+            assigner=dict(
+                type='MaxIoUAssigner',
+                pos_iou_thr=0.7,
+                neg_iou_thr=0.3,
+                min_pos_iou=0.3,
+                match_low_quality=True,
+                ignore_iof_thr=-1),
+            sampler=dict(
+                type='RandomSampler',
+                num=256,
+                pos_fraction=0.5,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=False),
+            allowed_border=-1,
+            pos_weight=-1,
+            debug=False),
+        rpn_proposal=dict(
+            nms_pre=2000,
+            max_per_img=1000,
+            nms=dict(type='nms', iou_threshold=0.7),
+            min_bbox_size=0),
+        rcnn=dict(
+            assigner=dict(
+                type='MaxIoUAssigner',
+                pos_iou_thr=0.5,
+                neg_iou_thr=0.5,
+                min_pos_iou=0.5,
+                match_low_quality=False,
+                ignore_iof_thr=-1),
+            sampler=dict(
+                type='RandomSampler',
+                num=512,
+                pos_fraction=0.25,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=True),
+            pos_weight=-1,
+            debug=False)),
+    test_cfg=dict(
+        rpn=dict(
+            nms_pre=2000,
+            max_per_img=1000,
+            nms=dict(type='nms', iou_threshold=0.7),
+            min_bbox_size=0),
+        rcnn=dict(
+            score_thr=0.01,
+            nms=dict(type='nms', iou_threshold=0.4),
+            max_per_img=100)))
+checkpoint_config = dict(interval=1)
+log_config = dict(interval=50, hooks=[dict(type='TextLoggerHook')])
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+opencv_num_threads = 0
+mp_start_method = 'fork'
+auto_scale_lr = dict(enable=True, base_batch_size=64)
+dataset_type = 'CocoDatasetOV'
+image_size = (640, 640)
+file_client_args = dict(backend='disk')
+train_pipeline = [
+    dict(type='LoadImageFromFile', file_client_args=file_client_args),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=False),
+    dict(
+        type='Resize',
+        img_scale=image_size,
+        ratio_range=(0.1, 2.0),
+        multiscale_mode='range',
+        keep_ratio=True),
+    dict(
+        type='RandomCrop',
+        crop_type='absolute_range',
+        crop_size=image_size,
+        recompute_bbox=True,
+        allow_negative_crop=True),
+    dict(type='FilterAnnotations', min_gt_bbox_wh=(0.01, 0.01)),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(
+        type='Normalize',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        to_rgb=True),
+    dict(type='Pad', size=image_size),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile', file_client_args=file_client_args),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=image_size,
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(
+                type='Normalize',
+                mean=[123.675, 116.28, 103.53],
+                std=[58.395, 57.12, 57.375],
+                to_rgb=True),
+            dict(type='Pad', pad_to_square=True),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img'])
+        ])
+]
+data = dict(
+    samples_per_gpu=8,
+    workers_per_gpu=8,
+    train=dict(
+        type=dataset_type,
+        ann_file=
+        'data/coco/zero-shot/instances_train2017_seen_2_65_cat.json',
+        img_prefix='data/coco/train2017/',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        ann_file='data/coco/zero-shot/instances_val2017_all_2.json',
+        img_prefix='data/coco/val2017/',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file='data/coco/zero-shot/instances_val2017_all_2.json',
+        img_prefix='data/coco/val2017/',
+        pipeline=test_pipeline)
+)
+evaluation = dict(interval=1, metric=['bbox'])
+optimizer = dict(type='AdamW', lr=0.0001, betas=(0.9, 0.999), weight_decay=0.1)
+optimizer_config = dict(grad_clip=dict(max_norm=1.0, norm_type=2))
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=250,
+    warmup_ratio=0.001,
+    step=[100])
+runner = dict(type='EpochBasedRunner', max_epochs=3)
+fp16 = dict(loss_scale=512.0)
+auto_resume = False
--- F-ViT/models/evaclip_vit.py
+++ F-ViT/models/evaclip_vit.py
@@ -10,13 +10,82 @@ from mmcv.utils.logging import print_log
 from mmcv.cnn import build_norm_layer
 
 
+def window_partition(x, window_size=16):
+    """
+    Partition into non-overlapping windows with padding if needed.
+    Args:
+        x (tensor): input tokens with [B, H, W, C].
+        window_size (int): window size.
+    Returns:
+        windows: windows after partition with [B * num_windows, window_size, window_size, C].
+        (Hp, Wp): padded height and width before partition
+    """
+    # 1024 -> 64 -> 16x4
+    # x: bs, 1 + h*w, c
+    x_cls = x[:, :1]   # bs, 1, c
+    x = x[:, 1:]
+    B, H_W, C = x.shape
+    H = W = int(H_W ** 0.5)
+    x = x.view(B, H, W, C)
+
+    pad_h = (window_size - H % window_size) % window_size
+    pad_w = (window_size - W % window_size) % window_size
+    assert pad_h == 0 and pad_w == 0, f"For now we do not allow additional padding"
+    # if pad_h > 0 or pad_w > 0:
+    #     x = F.pad(x, (0, 0, 0, pad_w, 0, pad_h))
+    Hp, Wp = H + pad_h, W + pad_w
+
+    x = x.view(B, Hp // window_size, window_size, Wp // window_size, window_size, C)
+    windows = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(-1, window_size * window_size, C)
+    x_cls = x_cls.repeat(1, Hp*Wp // int(window_size**2), 1).view(-1, 1, C)
+    windows = torch.cat([x_cls, windows], dim=1)
+
+    return windows, (Hp, Wp)
+
+def window_unpartition(windows, window_size, pad_hw, hw):
+    """
+    Window unpartition into original sequences and removing padding.
+    Args:
+        x (tensor): input tokens with [B * num_windows, 1+window_size*window_size, C].
+        window_size (int): window size.
+        pad_hw (Tuple): padded height and width (Hp, Wp).
+        hw (Tuple): original height and width (H, W) before padding.
+
+    Returns:
+        x: unpartitioned sequences with [B, 1+H*W, C].
+    """
+    Hp, Wp = pad_hw
+    H, W = hw
+
+    windows_cls = windows[:, :1]    # cls tokens
+    windows = windows[:, 1:]
+
+    B = windows.shape[0] // (Hp * Wp // window_size // window_size)
+    x = windows.view(B, Hp // window_size, Wp // window_size, window_size, window_size, -1)
+    x = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(B, Hp*Wp, -1)
+
+    windows_cls = windows_cls.view(B, Hp*Wp // int(window_size**2), 1, -1)
+
+    x_cls = windows_cls.mean(1)   # average all cls tokens
+    x = torch.cat([x_cls, x], dim=1)
+
+    assert Hp == H and Wp == W, "For now we do not allow additional padding"
+    # if Hp > H or Wp > W:
+    #     x = x[:, :H, :W, :].contiguous()
+    return x
+
+
+
 @BACKBONES.register_module()
 class EvaCLIPViT(BaseModule):
-    def __init__(self, model_name, pretrained, out_indices=[3, 5, 7, 11], norm_cfg=None):
+    def __init__(self, model_name, pretrained, out_indices=[3, 5, 7, 11], norm_cfg=None,
+                 window_size=16, window_block_indexes=[]):
         super().__init__()
         self.vit_layers = out_indices
         self.model_name = model_name
         self.pretrained = pretrained  # the pretrained .pt file
+        self.window_size = window_size
+        self.window_block_indexes = window_block_indexes
         clip_model = open_clip.create_model(model_name,
                                             pretrained="eva",
                                             cache_dir=pretrained)
@@ -89,7 +158,14 @@ class EvaCLIPViT(BaseModule):
 
             outs = []
             for i, blk in enumerate(visual.blocks[:-1]):
-                x = blk(x, rel_pos_bias=rel_pos_bias)
+                if i in self.window_block_indexes:
+                    x_windows, pad_hw = window_partition(x, window_size=self.window_size)
+                    x_windows = blk(x_windows, rel_pos_bias=rel_pos_bias)
+                    x = window_unpartition(x_windows, window_size=self.window_size,
+                                           hw=(h, w), pad_hw=pad_hw)
+                else:
+                    x = blk(x, rel_pos_bias=rel_pos_bias)
+                # x = blk(x, rel_pos_bias=rel_pos_bias)
                 if i in self.vit_layers:
                     outs.append(self._expand_x(x, h, w))
             x = visual.blocks[-1].forward_without_attn(x)
