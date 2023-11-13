238f689e4ed25e50c79b2fa1a6201e538013d7e3
wusize <wusize@sensetime.com>
2023-11-14 01:14:13 +0800

    retrieval

---
+++ scripts/test_eva_vitb16_retrieval.sh
@@ -0,0 +1,9 @@
+NAME=$1
+CHECKPOINT=$2
+torchrun --nproc_per_node 8 -m training.main --batch-size=1 \
+--model EVA02-CLIP-B-16 --pretrained eva --test-type retrieval --train-data="" \
+--val-data data/coco/annotations/panoptic_val2017.json \
+--embed-path metadata/coco_panoptic_clip_hand_craft_EVACLIP_ViTB16.npy \
+--val-image-root data/coco/val2017  --cache-dir $CHECKPOINT --extract-type="v2" \
+--name $NAME --downsample-factor 16 --det-image-size 1024 \
+--dist-cache-dir checkpoints/EVA02_CLIP_B_psz16_s8B.pt
