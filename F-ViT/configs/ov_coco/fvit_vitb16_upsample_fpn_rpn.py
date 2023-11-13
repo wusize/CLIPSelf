8a3cb17aad48365f715f75c1d8471d336f16d3e2
wusize <wusize@sensetime.com>
2023-11-13 10:41:57 +0800

    debug

--- F-ViT/configs/ov_coco/fvit_vitb16_upsample_fpn_rpn.py
+++ F-ViT/configs/ov_coco/fvit_vitb16_upsample_fpn_rpn.py
@@ -1,5 +1,69 @@
 _base_ = './fvit_vitb16_upsample_fpn_bs64_3e_ovcoco_eva_original.py'
-model = dict(type='FViTRPN')
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+model = dict(
+    type='FViTRPN',
+    _delete_=True,
+    backbone=dict(
+        type='EvaCLIPViT',
+        model_name='EVA02-CLIP-B-16',
+        pretrained='checkpoints/EVA02_CLIP_B_psz16_s8B.pt',
+        norm_cfg=norm_cfg,
+        out_indices=[3, 5, 7, 11]),
+    neck=dict(
+        type='FPN',
+        in_channels=[768, 768, 768, 768],
+        out_channels=256,
+        num_outs=5,
+        norm_cfg=norm_cfg),
+    rpn_head=dict(
+        type='RPNHead',
+        in_channels=256,
+        feat_channels=256,
+        anchor_generator=dict(
+            type='AnchorGenerator',
+            scales=[8],
+            ratios=[0.5, 1.0, 2.0],
+            strides=[4, 8, 16, 32, 64]),
+        bbox_coder=dict(
+            type='DeltaXYWHBBoxCoder',
+            target_means=[0.0, 0.0, 0.0, 0.0],
+            target_stds=[1.0, 1.0, 1.0, 1.0]),
+        loss_cls=dict(
+            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
+        loss_bbox=dict(type='L1Loss', loss_weight=1.0),
+        num_convs=2),
+    train_cfg=dict(
+        rpn=dict(
+            assigner=dict(
+                type='MaxIoUAssigner',
+                pos_iou_thr=0.7,
+                neg_iou_thr=0.3,
+                min_pos_iou=0.3,
+                match_low_quality=True,
+                ignore_iof_thr=-1),
+            sampler=dict(
+                type='RandomSampler',
+                num=256,
+                pos_fraction=0.5,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=False),
+            allowed_border=-1,
+            pos_weight=-1,
+            debug=False),
+        rpn_proposal=dict(
+            nms_pre=2000,
+            max_per_img=1000,
+            nms=dict(type='nms', iou_threshold=0.7),
+            min_bbox_size=0)
+    ),
+    test_cfg=dict(
+        rpn=dict(
+            nms_pre=2000,
+            max_per_img=1000,
+            nms=dict(type='nms', iou_threshold=0.7),
+            min_bbox_size=0)
+    )
+)
 dataset_type = 'CocoDataset'
 image_size = (640, 640)
 file_client_args = dict(backend='disk')
