9ccb53493a12f492e0beaa817061826c2ca76c07
wusize <wusize@sensetime.com>
2024-01-03 22:54:42 +0800

    debug

--- scripts/train_clipself_cc3m_llava_openai_vitl14.sh
+++ scripts/train_clipself_cc3m_llava_openai_vitl14.sh
@@ -1,6 +1,6 @@
 torchrun --nproc_per_node 8 -m training.main_llava --batch-size=2 --lr=1e-5 --wd=0.1 --epochs=6 --workers=4 \
 --model='openai/clip-vit-large-patch14-336' --warmup 1000 --dataset-type grid_distill  \
---train-data data/cc3m_orginal/cc3m_train_original_size_filtered.json \
+--train-data data/cc3m/cc3m_train_original_size_filtered.json \
 --train-image-root="" \
 --train-ceph-root="BJ16:s3://wusize/cc3m_original_size/cc3m" \
 --log-every-n-steps 50 \
