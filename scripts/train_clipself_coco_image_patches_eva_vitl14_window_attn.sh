3802ca3259595ba38def2291d6d955c1a1bec4e1
Size Wu (吴思泽) <72066971+wusize@users.noreply.github.com>
2024-02-05 10:55:22 +0800

    Update train_clipself_coco_image_patches_eva_vitl14_window_attn.sh

--- scripts/train_clipself_coco_image_patches_eva_vitl14_window_attn.sh
+++ scripts/train_clipself_coco_image_patches_eva_vitl14_window_attn.sh
@@ -5,5 +5,5 @@ torchrun --nproc_per_node 8 -m training.main --batch-size=2 --lr=1e-5 --wd=0.1 -
 --embed-path metadata/coco_panoptic_clip_hand_craft_EVACLIP_ViTL14x336.npy --train-image-root data/coco/train2017 \
 --val-image-root data/coco/val2017  --cache-dir checkpoints/EVA02_CLIP_L_336_psz14_s6B.pt --log-every-n-steps 50 \
 --lock-image --save-frequency 6 --lock-image-unlocked-groups 24 --extract-type="v2" \
---name clipself_coco_6_save6_test1_eva_vitl14_24layers --downsample-factor 14 --det-image-size 896 \
+--name clipself_coco_6_save6_test1_eva_wa_vitl14_24layers --downsample-factor 14 --det-image-size 896 \
 --alpha 0.95 --window-size 16 --window-attention
